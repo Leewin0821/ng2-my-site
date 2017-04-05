ecf020020647fda0c76d89ac03b625d6bc6da1cd
lwzhang <lwzhang@thoughtworks.com>
2017-04-05 21:19:18 +0800

    [Leewin][#4] Tried with configuration files again #2

--- build/build.js
+++ build/build.js
@@ -1,1 +1,9 @@
-console.log('Awesome')
+'use strict'
+let spawn = require('child_process').spawn;
+
+let sh = 'sh', shFlag = '-c', cmd = 'ng --version';
+
+spawn(sh, [shFlag, cmd], {
+  env: process.env,
+  stdio: ['pipe', process.stdout, process.stderr]
+});
